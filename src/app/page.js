8b55ab2bc4f34aaa4b5e406735d8dde90ee1a8dd
mutlugulerce <gulercemutlu1993@gmail.com>
2023-08-06 23:10:26 +0300

    klasörlerinı ve store ve slice oluşturulması

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+
+
+export default function Home() {
+  return (
+    <div>
+   Crud User Center
+    </div>
+  )
+}
---
+++ src/features/userSlice.js
@@ -0,0 +1,16 @@
+import {BASE_URL} from '../constans/api';
+import {createSlice} from '@reduxjs/toolkit';
+
+
+const initialState = {
+
+}
+
+export const userSlice = createSlice({
+    name: 'users',
+    initialState,
+    reducers:{ },
+})
+
+
+export default userSlice.reducer
